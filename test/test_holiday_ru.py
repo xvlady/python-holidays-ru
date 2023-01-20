d73f230c55387b9f24aab931100da04a2eed8cfe
Vladimir.Khonin <Vladimir.Khonin@nexign.com>
2023-01-20 08:20:31 +0500

    fix black

--- test/test_holiday_ru.py
+++ test/test_holiday_ru.py
@@ -12,10 +12,16 @@ class TestBasics(unittest.TestCase):
         self.assertFalse(
             check_holiday(date(2022, 12, 31), with_weekends=False)
         )  # False, it's Sunday
-        self.assertEqual(is_holiday(date(2023, 1, 1)), "Новогодние каникулы")  # "New Year's Day"
+        self.assertEqual(
+            is_holiday(date(2023, 1, 1)), "Новогодние каникулы"
+        )  # "New Year's Day"
         self.assertEqual(is_holiday(date(2023, 1, 12)), "")  # ""
-        self.assertEqual(is_holiday(date(2023, 1, 15)), "Выходной")  # "Weekend"
-        self.assertEqual(is_holiday(date(2021, 12, 31)), "Передвинутый выходной")  # "Moved weekend"
+        self.assertEqual(
+            is_holiday(date(2023, 1, 15)), "Выходной"
+        )  # "Weekend"
+        self.assertEqual(
+            is_holiday(date(2021, 12, 31)), "Передвинутый выходной"
+        )  # "Moved weekend"
         self.assertEqual(
             is_holiday(date(2023, 1, 12)), ""
         )  # "", it's work Sunday
